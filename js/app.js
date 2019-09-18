256db76310fabf69764067672f3696fd5db69312
abdsamadf <abdulsamadf97@gmail.com>
2019-09-18 22:54:10 +0500

    add list in drawer

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
-// Here's my data model
-var ViewModel = function (first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
+const placeModel = [
+    { title: 'Clifton Sea View', location: { lat: 24.789667, lng: 67.04386}},
+    { title: 'Mazaar–e–Quaid', location: { lat: 24.874553, lng: 67.039813 }},
+    { title: 'Mohatta Palace', location: { lat: 24.814236, lng: 67.032993 } },
+]
 
-    this.fullName = ko.pureComputed(function () {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
+// place data model
+const Place = function(data) {
+    this.title = ko.observable(data.title);
+    this.lat = ko.observable(data.location.lat);
+    this.lng = ko.observable(data.location.lng);
+}
+
+// Here's my view model
+const ViewModel = function () {
+    let self = this;
+    this.placeList = ko.observableArray([]);
+
+    placeModel.forEach((item) => {
+        this.placeList.push(new Place(item));
+    })
+    // console.log(this.listItem)
+
 };
 
-ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+ko.applyBindings(new ViewModel()); // This makes Knockout get to work
