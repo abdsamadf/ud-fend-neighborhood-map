2c4720d3fde2e6e59c5ab065fa19959c409c337c
abdsamadf <abdulsamadf97@gmail.com>
2019-11-02 18:49:08 +0500

    add places using the Foursquare API

--- js/app.js
+++ js/app.js
@@ -1,19 +1,52 @@
+// Recommended class is to store data from Foursquare API
+class RecommendedPlace {
+    constructor(placeJSON) {
+        this.address = placeJSON.venue.location.address;
+        this.placeName = placeJSON.venue.name;
+        this.location = { lat: placeJSON.venue.location.lat, lng: placeJSON.venue.location.lng};
+        this.categoryType = placeJSON.venue.categories[0].name;
+    }
+}
+
+// retrieve places asynchronously from Foursquare exploreApi
+function retrievePlacesFoursquareAPI() {
+    const client_id = 'HDUNL52NGCNNDRRSWIVAXVT2DT3FHJMIPIJQ4XHHFD32J2IL';
+    const client_secret = 'QC4CKK0Z2X14QZSQ1A0IRZEWJGGZ10UE4TMSPSRAWTOA05HU';
+    const api_version = '20191013';
+
+    // https://developer.foursquare.com/docs/api/venues/explore
+    const exploreAPI = 'https://api.foursquare.com/v2/venues/explore';
+    const nearCity = 'Karachi';
+    const query = 'food';
+    const venueRecommendationRequestURL = `${exploreAPI}?client_id=${client_id}&client_secret=${client_secret}&v=${api_version}&near=${nearCity}&query=${query}&limit=10`;
+
+    return fetch(venueRecommendationRequestURL)
+           .then(response => response.json())
+           .then(json => json.response.groups[0].items)
+           .then(items => items.filter(item => item.venue.location.address != undefined))
+           .catch(() => window.alert("failed to load"));
+}
+
 let map;
 // Create a new blank array for all the listing markers.
 const markers = [];
 let infoWindow = null;
 
-const placeModel = [
-    { title: 'Clifton Sea View', location: { lat: 24.789667, lng: 67.04386}},
-    { title: 'Mazaar–e–Quaid', location: { lat: 24.874553, lng: 67.039813 }},
-    { title: 'Mohatta Palace', location: { lat: 24.814236, lng: 67.032993 } },
-    { title: 'Charna Island', location: { lat: 24.8967716, lng: 66.594808 } },
-    { title: 'Barrett Hodgson University', location: { lat: 24.7994988, lng: 67.1091056 } },
-];
+// Create a new blank array for all the recommended places.
+const placeModel = [];
+
+async function initPlaces() {
+    const json = await retrievePlacesFoursquareAPI();
+
+    json.forEach(item => {
+        placeModel.push(new RecommendedPlace(item));
+    });
+}
+
 
 // place data model
 const Place = function(data) {
-    this.title = ko.observable(data.title);
+    this.title = ko.observable(data.placeName);
     this.lat = ko.observable(data.location.lat);
     this.lng = ko.observable(data.location.lng);
 };
@@ -79,8 +112,6 @@ const ViewModel = function () {
     };
 };
 
-ko.applyBindings(new ViewModel()); // This makes Knockout get to work
-
 
 function setMarkerAnimation(marker) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
@@ -88,7 +119,8 @@ function setMarkerAnimation(marker) {
     setTimeout(() => marker.setAnimation(null), 1500);
 }
 
-function initMap() {
+async function initMap() {
+    await initPlaces();
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: placeModel[0].location.lat, lng: placeModel[0].location.lng},
         zoom: 13,
@@ -99,11 +131,16 @@ function initMap() {
     for (let i = 0; i < placeModel.length; i++) {
         // Get the position from the location array.
         const position = placeModel[i].location;
-        const title = placeModel[i].title;
+        const placeName = placeModel[i].placeName;
+        const address = placeModel[i].address;
+        const categoryType = placeModel[i].categoryType;
+
         // Create a marker per location, and put into markers array.
         const marker = new google.maps.Marker({
             position: position,
-            title: title,
+            title: placeName,
+            address: address,
+            category: categoryType,
             animation: google.maps.Animation.DROP,
             id: i
         });
@@ -125,6 +162,7 @@ function initMap() {
         bounds.extend(markers[i].position);
     }
     map.fitBounds(bounds);
+    ko.applyBindings(new ViewModel(placeModel)); // This makes Knockout get to work
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -140,7 +178,7 @@ function populateInfoWindow(marker, infowindow) {
         infowindow.addListener('closeclick', function () {
             infowindow.marker = null;
         });
-        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.setContent('<div>' + marker.title + '<p>Address: ' + marker.address + '<br>Category Type: ' + marker.category + '</p>' + '</div>');
         // Open the infowindow on the correct marker.
         infowindow.open(map, marker);
     }
